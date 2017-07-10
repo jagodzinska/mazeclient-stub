4f0bb1fb38e796c339601810658fad815450f374
Marco Jagodzinska <jagodzinska@fh-aachen.de>
2017-07-10 13:01:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Dieses Grundgerüst wird in Jülich in Zukunft dafür benutzt den Studis für ihren Mazenetclient ein paar Dinge vorzugeben:
+* Maven
+* Java 8
+* UTF8
+* pom-config für `mvn exec:java`
+
+In den gleichen Namespace forken funktioniert nicht, deshalb muss dafür eine andere Lösung gefunden werden
---
+++ src/main/java/de/fhac/rn/mazenet/client/Client.java
@@ -0,0 +1,19 @@
+package de.fhac.rn.mazenet.client;
+
+/**
+ * Teammitglieder: Person A, Person B, ...
+ * 
+ * TODO: Der Klassennamen muss zum Teamnamen refactort werden
+ */
+public class Client {
+
+    // TODO
+    private static final String TEAMNAME = "hier Teamnamen eintragen";
+
+    public static void main(String[] args) {
+        System.out.println("----------");
+        System.out.println("clientstub");
+        System.out.println("----------");
+    }
+
+}
