bb03f18feae436dc76c76788c0378117e83d0c3a
Marco Jagodzinska <jagodzinska@fh-aachen.de>
2017-07-16 20:54:42 +0200

    * commandlineparser (mvn) * jar-generierung (mvn)

--- src/main/java/de/fhac/rn/mazenet/client/Client.java
+++ src/main/java/de/fhac/rn/mazenet/client/Client.java
@@ -1,5 +1,11 @@
 package de.fhac.rn.mazenet.client;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * Teammitglieder: Person A, Person B, ...
  * 
@@ -9,11 +15,29 @@ public class Client {
 
     // TODO
     private static final String TEAMNAME = "hier Teamnamen eintragen";
+    private static String HOSTNAME = "localhost";
+    private static int PORT = 5123;
+    private static String TRUSTSTORE_PATH = "";
+    private static String hostnameOptionShortName = "h";
+    private static String portOptionsShortName = "p";
+    private static String truststorePathShortOptionName = "tp";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         System.out.println("----------");
         System.out.println("clientstub");
         System.out.println("----------");
+        Options options = new Options();
+        options.addOption(hostnameOptionShortName, true, "Host");
+        options.addOption(portOptionsShortName, true, "Port");
+        options.addOption(truststorePathShortOptionName, true, "Truststore path");
+        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine commandLine = commandLineParser.parse(options, args);
+        if (commandLine.hasOption(hostnameOptionShortName))
+            HOSTNAME = commandLine.getOptionValue(hostnameOptionShortName);
+        if (commandLine.hasOption(portOptionsShortName))
+            PORT = Integer.parseInt(commandLine.getOptionValue(portOptionsShortName));
+        if (commandLine.hasOption(truststorePathShortOptionName))
+            TRUSTSTORE_PATH = commandLine.getOptionValue(truststorePathShortOptionName);
     }
 
 }
